451481c287cf15c4e90ca2a4983c9012e8c609b6
yzyzdkami <zch2166202772@163.com>
2023-08-26 10:37:52 +0800

    牡蛎

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function navigateTo(url) {
-    window.open(url, '_blank');
+    window.open(url, '_blank'); // 在新窗口或新标签页中打开链接
+}
+
+function navigateToPage(pageName) {
+    window.open(pageName + '.html', '_blank'); // 在新窗口或新标签页中打开页面
 }
